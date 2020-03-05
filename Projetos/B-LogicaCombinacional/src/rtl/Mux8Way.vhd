3d817140a115c8a8f0742b1faac677eac5026579
thalials <thalials@al.insper.edu.br>
2020-03-05 14:52:16 -0300

    add mux8way

--- Projetos/B-LogicaCombinacional/src/rtl/Mux8Way.vhd
+++ Projetos/B-LogicaCombinacional/src/rtl/Mux8Way.vhd
@@ -20,8 +20,8 @@ begin
 	q   <= 	a when sel="000" else
 		 	b when sel="001" else
 			c when sel="010" else
-			d when sel="100" else
-			e when sel="011" else
+			d when sel="011" else
+			e when sel="100" else
 			f when sel="101" else
 			g when sel="110" else
 			h when sel="111";
