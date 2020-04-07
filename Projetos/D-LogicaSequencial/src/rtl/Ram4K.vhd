22fd6e6b5318248481daf498e5ac31afc2d4f32d
luizfelipelazzaron <luizfl@al.insper.edu.br>
2020-04-07 14:08:44 -0300

    Ram4k and PCfuncionando

--- Projetos/D-LogicaSequencial/src/rtl/Ram4K.vhd
+++ Projetos/D-LogicaSequencial/src/rtl/Ram4K.vhd
@@ -59,6 +59,17 @@ architecture arch of Ram4K is
 	signal output0, output1, output2, output3, output4, output5, output6, output7 : STD_LOGIC_VECTOR(15 downto 0);
 
 begin
+	DEMUX0: DMux8Way port map(load, address(8 downto 6), load0, load1, load2, load3, load4, load5, load6, load7);
 
+	ram4k0: Ram512 port map (clock => clock, input => input, load => load0, address => address( 8 downto 0), output => output0);
+	ram4k1: Ram512 port map (clock => clock, input => input, load => load1, address => address( 8 downto 0), output => output1);
+	ram4k2: Ram512 port map (clock => clock, input => input, load => load2, address => address( 8 downto 0), output => output2);
+	ram4k3: Ram512 port map (clock => clock, input => input, load => load3, address => address( 8 downto 0), output => output3);
+	ram4k4: Ram512 port map (clock => clock, input => input, load => load4, address => address( 8 downto 0), output => output4);
+	ram4k5: Ram512 port map (clock => clock, input => input, load => load5, address => address( 8 downto 0), output => output5);
+	ram4k6: Ram512 port map (clock => clock, input => input, load => load6, address => address( 8 downto 0), output => output6);
+	ram4k7: Ram512 port map (clock => clock, input => input, load => load7, address => address( 8 downto 0), output => output7);
+
+	MUX0: Mux8Way16 port map(output0, output1, output2, output3, output4, output5, output6, output7, address(8 downto 6), output);
 
 end architecture;
